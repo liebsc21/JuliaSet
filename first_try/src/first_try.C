f1df449b1353b3ca902d724593bb01c1c446a2f2
liebsch <sebastian-liebschner@gmx.de>
2018-10-30 14:20:09 +0100

    can produce first csv-file

--- first_try/src/first_try.C
+++ first_try/src/first_try.C
@@ -1,26 +1,53 @@
-//#include <boost/program_options.hpp>
-//namespace po = boost::program_options;
-//#include <boost/property_tree/ptree.hpp>
-//#include <boost/property_tree/ini_parser.hpp>
-//
 #include <iostream>
 //#include <iterator>
 using namespace std;
 
 #include "init.H"
 #include "point.H"
+#include <fstream>
+#include <chrono>
+//using namespace std::chrono_literals;
 
 
 int main(int argc, char* argv[]){
 
   try{
+    auto start = chrono::steady_clock::now();
+
     Init init(argc, argv);
-    const double n   = init.get_n();
+    const int    n   = init.get_n();
     const double r   = init.get_r();
     const double phi = init.get_phi();
+    const double xmin = init.get_xmin();
+    const double xmax = init.get_xmax();
+    const double ymin = init.get_ymin();
+    const double ymax = init.get_ymax();
+    const double gridwidth = init.get_gridwidth();
+    const double delta_x = (xmax-xmin)/gridwidth;
+    const double delta_y = (ymax-ymin)/gridwidth;
+
     Point point(n,r,phi);
-    point.set_point(0.5,0.5);
-    point.get_it();
+    ofstream myfile;
+    myfile.open("output_1.csv");
+
+    for(int i_x=0; i_x<=gridwidth; i_x++){
+      double x = xmin + i_x*delta_x;
+      for(int i_y=0; i_y<=gridwidth; i_y++){
+        double y = ymax - i_y*delta_y;
+//        cout << "x = " << x << ", y = " << y << "\n";
+        point.set_point(x,y);
+        int it = point.get_it();
+        if (i_y==0) myfile << it;
+        else myfile << "," << it;
+      }
+      myfile << "\n";
+    }
+    myfile.close();
+
+    auto end = chrono::steady_clock::now();
+    cout << "Elapsed time = " <<
+    chrono::duration_cast<chrono::milliseconds>(end-start).count() << 
+    " miliseconds\n";
   }
   catch(exception& e) {
     cerr << "error: " << e.what() << "\n";
--- first_try/src/point.C
+++ first_try/src/point.C
@@ -3,17 +3,20 @@
 #include "point.H"
 
 void Point::set_point(const double &re, const double &im){
-  m_z  = (re,im);
+  m_z  = {re,im};
+//    cout << "z = " << m_z << "\n";
   m_it = 0;
 }
 
 complex<double> Point::f(){
   m_it++;
-  return pow(m_z,m_n) + m_r*exp(I*m_phi);
+  m_z = pow(m_z,m_n) + m_r*exp(I*m_phi);
 }
 
 int Point::get_it(){
   while(abs(m_z)<=2 && m_it<=300) {
+//    cout << "z = " << m_z << "\n";
+//    cout << "|z| = " << abs(m_z) << "\n";
     f();
 //    cout << "Number of iterations = " << m_it << "\n" ;
   }
