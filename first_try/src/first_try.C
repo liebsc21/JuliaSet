cafe656bd6e540729de00e2cead6470d61a6d587
liebsch <sebastian-liebschner@gmx.de>
2019-01-18 21:09:01 +0100

    restructured code (created Grid-class)

--- first_try/src/first_try.C
+++ first_try/src/first_try.C
@@ -3,162 +3,47 @@ using namespace std;
 
 #include "init.H"
 #include "point.H"
-#include <fstream>
+#include "grid.H"
 #include <chrono>
-#include <string>
-#include <sstream>
-#include <iomanip>
-#include <list>
+//#include <list>
+
+
+
 
 
 int main(int argc, char* argv[]){
 
-  try{
-    auto start = chrono::steady_clock::now();
+try{
+  auto start = chrono::steady_clock::now();
 
-    Init init(argc, argv);
-    const int    n   = init.get_n();
-    const double r_c   = init.get_r();
-    const double phi_c = init.get_phi();
-    const double xmin = init.get_xmin();
-    const double xmax = init.get_xmax();
-    const double ymin = init.get_ymin();
-    const double ymax = init.get_ymax();
-    const double max_radius2 = min(xmax*xmax,ymax*ymax);
-    const double min_radius2 = (n==11) ? 0.002*max_radius2
-                                       : 0.001*max_radius2;
-    const double gridwidth = init.get_gridwidth();
-    const double delta_x = gridwidth==1 ? 0:(xmax-xmin)/(gridwidth-1);
-    const double delta_y = gridwidth==1 ? 0:(ymax-ymin)/(gridwidth-1);
-    /* this is how Carsten calculates it */
-//    const double delta_x = (xmax-xmin)/(gridwidth);
-//    const double delta_y = (ymax-ymin)/(gridwidth);
-    const int N = init.get_N();
-    const double alpha = 2.*M_PI/n;
-    const double eps1   = (n==2)||(n==4) ? 0. : 4.*M_PI/180;
-    const double eps2   = (n==8) ? 6.*M_PI/180 : 0;
-    const bool debug = init.get_debug();
+  Grid grid(argc, argv);
+  int N = grid.get_N();
 
-    if(debug) cout<<"alpha = " << alpha << "\n";
-    if(debug) cout<<"eps1 = " << eps1 << "\n";
-    if(debug) cout<<"eps2 = " << eps1 << "\n";
-
-    /* iterate over steps, each producing one csv-file */
-    for(int t=0; t<N/2; t++){
-      vector<vector<int>> grid(gridwidth,
-                             vector<int>(gridwidth,0));
-      vector<vector<int*>> grid_p(gridwidth,vector<int*>(gridwidth));
-
-      Point point(n,r_c,phi_c+2*M_PI*t/N);
-
-      for(int i_y=0; i_y<gridwidth; i_y++){
-        double y = ymax - i_y*delta_y;
-
-        for(int i_x=0; i_x<gridwidth; i_x++){
-          double x = xmin + i_x*delta_x;
-
-          /* associate grid of pointers with original grid */
-          grid_p[i_y][i_x] = &grid[i_y][i_x];
-          point.set_point(x,y);
-          double phi = point.get_phi();
-          double radius2 = x*x+y*y;
-          /* fill zeroth sector and area outside circle */
-          if (radius2>max_radius2 
-              || (0.<=phi && phi<=alpha+eps1)
-              || phi>2.*M_PI-eps2
-              || radius2<min_radius2 ){
-            int it = point.get_it();
-            grid[i_y][i_x] = it;
-          }
-          else{
-            /* fill missing sectors */
-            int sector = point.get_phi()/alpha;
-            double phi_org = phi-sector*alpha;
-            double r_org   = point.get_r();
-            double x_org = r_org*cos(phi_org);
-            double y_org = r_org*sin(phi_org);
-            int i_x_org = round((x_org-xmin)/delta_x);
-            int i_y_org = round((ymax-y_org)/delta_y);
-            if(debug && i_y==45 && i_x==45){
-              cout<<"(x,y) = (" << x << "," << y << ")\n";
-              cout<<"phi = " << phi << "\n";
-              cout<<"(i_x,i_y) = (" << i_x << "," << i_y <<")\n";
-              cout<< "sector = " << sector << "\n";
-              cout<<"(x_org,y_org) = (" << x_org << "," 
-                  << y_org << ")\n";
-              cout<< "phi_org = " << phi_org << "\n";
-              cout<<"(i_x_org,i_y_org) = (" << i_x_org << ","
-                  << i_y_org <<")\n";
-              cout << "--------------\n";
-            }
-            grid_p[i_y][i_x] = &grid[i_y_org][i_x_org];
-          }
-        }
-      }
-
-      if(debug){
-        for(int i_y=0; i_y<gridwidth; i_y++){
-          double y = ymax - i_y*delta_y;
-          for(int i_x=0; i_x<gridwidth; i_x++){
-            double x = xmin + i_x*delta_x;
-            if (x*x+y*y<max_radius2 && *grid_p[i_y][i_x]==0) 
-              cout << "i_y = " << i_y
-                   << "i_x = " << i_x << "\n";
-          }
-        }
-      }
-
-
-      /* write to file 1-N/2 */
-      ofstream myfile1, myfile2;
-      stringstream ss1, ss2;
-      ss1 << setw(3) << setfill('0') << t;
-      ss2 << setw(3) << setfill('0') << N-1-t;
-      cout << ss1.str() << "\n";
-      myfile1.open("output"+ss1.str()+".csv");
-      for(vector<vector<int*>>::iterator yrow_it=grid_p.begin();
-          yrow_it!=grid_p.end(); ++yrow_it){
-        for(vector<int*>::iterator xrow_it=yrow_it->begin();
-            xrow_it!=yrow_it->end(); ++xrow_it){
-          if (xrow_it!=yrow_it->begin()) myfile1 << "," << **xrow_it;
-          else myfile1 << **xrow_it;
-        }
-        myfile1 << "\n";
-      }
-      myfile1.close();
-
-      /* write to file N/2-N*/
-      myfile2.open("output"+ss2.str()+".csv");
-      for(vector<vector<int*>>::reverse_iterator yrow_rit=grid_p.rbegin();
-          yrow_rit!=grid_p.rend(); ++yrow_rit){
-        for(vector<int*>::iterator xrow_it=yrow_rit->begin();
-            xrow_it!=yrow_rit->end(); ++xrow_it){
-          if (xrow_it!=yrow_rit->begin()) myfile2 << "," << **xrow_it;
-          else myfile2 << **xrow_it;
-        }
-        myfile2 << "\n";
-      }
-      myfile2.close();
-    }
-
-    auto end = chrono::steady_clock::now();
-    cout << "Elapsed time = " <<
-    chrono::duration_cast<chrono::milliseconds>(end-start).count()/1000. << 
-    " seconds\n";
-  }
-  catch(exception& e) {
-    cerr << "error: " << e.what() << "\n";
-    cerr << "type '--help' to view options\n";
-    return 1;
-  }
-  catch(int &i)  /* exception, if help-option is given */ 
-  {
-    return 1;
-  }
-  catch(...) {
-    cerr << "Exception of unknown type!\n";
-    return 1;
+  /* iterate over steps, each producing one csv-file */
+  for(int t=0; t<N/2; t++){
+    grid.calc_grid(t);
+    grid.write_to_file(t);
   }
 
-  return 0;
+  auto end = chrono::steady_clock::now();
+  cout << "Elapsed time = " <<
+  chrono::duration_cast<chrono::milliseconds>(end-start).count()
+  /1000. << " seconds\n";
+}
+
+catch(exception& e) {
+  cerr << "error: " << e.what() << "\n";
+  cerr << "type '--help' to view options\n";
+  return 1;
+}
+catch(int &i)  /* exception, if help-option is given */ 
+{
+  return 1;
+}
+catch(...) {
+  cerr << "Exception of unknown type!\n";
+  return 1;
+}
+
+return 0;
 }
---
+++ first_try/src/grid.C
@@ -0,0 +1,157 @@
+#include <iostream>
+
+#include <sstream>
+#include <string>
+#include <iomanip>
+#include "grid.H"
+#include "init.H"
+#include "point.H"
+
+using namespace std;
+
+Grid::Grid(int argc, char* argv[]){
+
+    Init init(argc, argv);
+    n   = init.get_n();
+    r_c   = init.get_r();
+    phi_c = init.get_phi();
+    xmin = init.get_xmin();
+    xmax = init.get_xmax();
+    ymin = init.get_ymin();
+    ymax = init.get_ymax();
+    max_radius2 = min(xmax*xmax,ymax*ymax);
+    min_radius2 = (n==11) ? 0.002*max_radius2 : 0.001*max_radius2;
+    gridwidth = init.get_gridwidth();
+    delta_x = gridwidth==1 ? 0:(xmax-xmin)/(gridwidth-1);
+    delta_y = gridwidth==1 ? 0:(ymax-ymin)/(gridwidth-1);
+    /* this is how Carsten calculates it */
+//    delta_x = (xmax-xmin)/(gridwidth);
+//    delta_y = (ymax-ymin)/(gridwidth);
+    N = init.get_N();
+    alpha = 2.*M_PI/n;
+    eps1   = (n==2)||(n==4) ? 0. : 4.*M_PI/180;
+    eps2   = (n==8) ? 6.*M_PI/180 : 0;
+    debug = init.get_debug();
+
+    grid_p.resize(gridwidth);
+    for(int i=0; i<gridwidth; i++){
+      grid_p[i].resize(gridwidth);
+    }
+
+//    if(debug) cout<<"alpha = " << alpha << "\n";
+//    if(debug) cout<<"eps1 = " << eps1 << "\n";
+//    if(debug) cout<<"eps2 = " << eps1 << "\n";
+}
+
+
+
+void Grid::calc_grid(const int t){ 
+
+  static vector<vector<int>> grid(gridwidth,
+                         vector<int>(gridwidth,0));
+
+  Point point(n,r_c,phi_c+2*M_PI*t/N);
+
+  for(int i_y=0; i_y<gridwidth; i_y++){
+    double y = ymax - i_y*delta_y;
+
+    for(int i_x=0; i_x<gridwidth; i_x++){
+      double x = xmin + i_x*delta_x;
+
+      /* associate grid of pointers with original grid */
+      grid_p[i_y][i_x] = &grid[i_y][i_x];
+      point.set_point(x,y);
+      double phi = point.get_phi();
+      double radius2 = x*x+y*y;
+      /* fill zeroth sector and area outside circle */
+      if (radius2>max_radius2 
+          || (0.<=phi && phi<=alpha+eps1)
+          || phi>2.*M_PI-eps2
+          || radius2<min_radius2 ){
+        int it = point.get_it();
+        grid[i_y][i_x] = it;
+      }
+      else{
+        /* fill missing sectors */
+        int sector = point.get_phi()/alpha;
+        double phi_org = phi-sector*alpha;
+        double r_org   = point.get_r();
+        double x_org = r_org*cos(phi_org);
+        double y_org = r_org*sin(phi_org);
+        int i_x_org = round((x_org-xmin)/delta_x);
+        int i_y_org = round((ymax-y_org)/delta_y);
+        if(debug && i_y==45 && i_x==45){
+          cout<<"(x,y) = (" << x << "," << y << ")\n";
+          cout<<"phi = " << phi << "\n";
+          cout<<"(i_x,i_y) = (" << i_x << "," << i_y <<")\n";
+          cout<< "sector = " << sector << "\n";
+          cout<<"(x_org,y_org) = (" << x_org << "," 
+              << y_org << ")\n";
+          cout<< "phi_org = " << phi_org << "\n";
+          cout<<"(i_x_org,i_y_org) = (" << i_x_org << ","
+              << i_y_org <<")\n";
+          cout << "--------------\n";
+        }
+        grid_p[i_y][i_x] = &grid[i_y_org][i_x_org];
+      }
+    }
+  }
+  if(debug){
+    for(int i_y=0; i_y<gridwidth; i_y++){
+//      double y = ymax - i_y*delta_y;
+      for(int i_x=0; i_x<gridwidth; i_x++){
+//        double x = xmin + i_x*delta_x;
+//        if (x*x+y*y<max_radius2 && *grid_p[i_y][i_x]==0) 
+          cout << "*grid_p[" << i_y << ", "<< i_x << "] = " 
+               << *grid_p[i_y][i_x] << "\n";
+      }
+    }
+  }
+}
+
+void Grid::write_to_file(const int t){
+
+  if(debug){
+    cout << "write out\n";
+    for(int i_y=0; i_y<gridwidth; i_y++){
+//      double y = ymax - i_y*delta_y;
+      for(int i_x=0; i_x<gridwidth; i_x++){
+//        double x = xmin + i_x*delta_x;
+//        if (x*x+y*y<max_radius2 && *grid_p[i_y][i_x]==0) 
+          cout << "*grid_p[" << i_y << ", "<< i_x << "] = " 
+               << *grid_p[i_y][i_x] << "\n";
+      }
+    }
+  }
+
+  /* write to file 1-N/2 */
+  ofstream myfile1, myfile2;
+  stringstream ss1, ss2;
+  ss1 << setw(3) << setfill('0') << t;
+  ss2 << setw(3) << setfill('0') << N-1-t;
+  cout << ss1.str() << "\n";
+  myfile1.open("output"+ss1.str()+".csv");
+  for(vector<vector<int*>>::iterator yrow_it=grid_p.begin();
+      yrow_it!=grid_p.end(); ++yrow_it){
+    for(vector<int*>::iterator xrow_it=yrow_it->begin();
+        xrow_it!=yrow_it->end(); ++xrow_it){
+      if (xrow_it!=yrow_it->begin()) myfile1 << "," << **xrow_it;
+      else myfile1 << **xrow_it;
+    }
+    myfile1 << "\n";
+  }
+  myfile1.close();
+
+  /* write to file N/2-N*/
+  myfile2.open("output"+ss2.str()+".csv");
+  for(vector<vector<int*>>::reverse_iterator yrow_rit=grid_p.rbegin();
+      yrow_rit!=grid_p.rend(); ++yrow_rit){
+    for(vector<int*>::iterator xrow_it=yrow_rit->begin();
+        xrow_it!=yrow_rit->end(); ++xrow_it){
+      if (xrow_it!=yrow_rit->begin()) myfile2 << "," << **xrow_it;
+      else myfile2 << **xrow_it;
+    }
+    myfile2 << "\n";
+  }
+  myfile2.close();
+}
